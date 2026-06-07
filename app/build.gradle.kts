09db0487456b4a4c0a5d3fa9c83d2ec5fb3fce63
liburce0412-alt <liburce0412@gmail.com>
2026-06-07 11:30:30 +0800

    chore: 升级版本号至 1.1 (versionCode 2)
    修复首页导航 bug 后，需要递增 versionCode 才能被系统识别为更新。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,4 @@
-plugins {
+﻿plugins {
   alias(libs.plugins.android.application)
   alias(libs.plugins.kotlin.compose)
   alias(libs.plugins.kotlin.serialization)
@@ -15,8 +15,8 @@ android {
     applicationId = "com.aistudio.campusai.abcxyz"
     minSdk = 24
     targetSdk = 36
-    versionCode = 1
-    versionName = "1.0"
+    versionCode = 2
+    versionName = "1.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
